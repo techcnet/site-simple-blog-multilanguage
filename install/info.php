139037c04a03574d2be00b9da1a5cee4e7bd8d7e
techcnet <70081687+techcnet@users.noreply.github.com>
2022-02-24 11:16:07 +0700

    Delete install directory

--- install/info.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined("PROCESSWIRE_INSTALL")) die();
-$info = array(
-	'title' => "Simple Multilingual Blog Profile for ProcessWire 3x", 
-	'summary' => "Site profile for ProcessWire which offers a simple, multilingual and responsive blog without any third-party modules.", 
-	'screenshot' => "screenshot.png"
-	);
