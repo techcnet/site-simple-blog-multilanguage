9809c57a2192d5565235ec181d52724db0cd7ecf
techcnet <70081687+techcnet@users.noreply.github.com>
2023-08-29 17:21:45 +0700

    Add files via upload

---
+++ install/info.php
@@ -0,0 +1,7 @@
+<?php namespace ProcessWire;
+if(!defined("PROCESSWIRE_INSTALL")) die();
+$info = array(
+	'title' => "Simple and Multilingual Blog Profile for ProcessWire 3x", 
+	'summary' => "This site profile for ProcessWire offers a simple, multilingual and responsive blog which doesn't require any module. It works exclusively with some template files.", 
+	'screenshot' => "screenshot.jpg"
+);
